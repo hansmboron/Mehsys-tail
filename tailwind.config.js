81c191ea71462744a5548ad2bbd977bb53af5fd7
Hans M. Boron <hansmateusboron@gmail.com>
2021-01-29 21:33:54 -0300

    primeiro commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          500: '#850085',
+          400: '#991099',
+        }
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
+}
